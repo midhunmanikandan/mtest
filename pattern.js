3ecaffeeb130e67119b9203305d37e14dac87a95
midhunmanikandan <midhunmanikandan870@gmail.com>
2024-05-24 11:22:52 +0530

    first commit

---
+++ pattern.js
@@ -0,0 +1,19 @@
+let rows=5;
+let pattern="";
+
+for (let i=0;i <= rows;i++){
+    for (let j =1|| j==rows;j<i;j++){
+        pattern+="*";
+        {
+        pattern+=""
+        }
+}
+}
+console.log(pattern);
+    
+     
+
+
+
+
+
